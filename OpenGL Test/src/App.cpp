e8a6aca9c347fc7487eeeceafc22c197f9fbfe88
fl2mexd <kaidenchrisbird@gmail.com>
2023-03-26 01:31:17 +1100

    Time to rewrite :^)

--- OpenGL Test/src/App.cpp
+++ OpenGL Test/src/App.cpp
@@ -1,125 +1,224 @@
 #include <GL/glew.h> // GLEW
 #include <GLFW/glfw3.h> // GLFW
-#include <iostream> // std library
 
-// Abstracted OpenGL calls
-#include "Renderer.h"
-#include "VertexBuffer.h"
-#include "VertexBufferLayout.h"
-#include "IndexBuffer.h"
-#include "VertexArray.h"
-#include "Shader.h"
-#include "Texture.h"
-
-#include "GLM/glm.hpp" // GLM
-#include "GLM/gtc/matrix_transform.hpp" //GLM Matrices
+#include <iostream>
 
 // ImGui
 #include "ImGui/imgui.h"
 #include "ImGui/imgui_impl_glfw.h"
 #include "ImGui/imgui_impl_opengl3.h"
 
-// Tests
-#include "Tests/TestClearColor.h"
-#include "Tests/TestTex2D.h"
+bool isWireframe = false;
+
+void framebufferSizeCallback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, true);
+        std::cout << "Escape Key pressed, Exiting program..." << std::endl;
+    }
+    if (key == GLFW_KEY_TAB && action == GLFW_PRESS)
+    {
+        isWireframe = !isWireframe;
+        std::cout << "Wireframe Toggled" << std::endl;
+    }
+}
+
+const char* vertexShaderSource = R"glsl(
+#version 330 core
+layout (location = 0) in vec3 aPos;   // the position variable has attribute position 0
+layout (location = 1) in vec3 aColor; // the color variable has attribute position 1
+  
+out vec3 ourColor; // output a color to the fragment shader
+
+void main()
+{
+    gl_Position = vec4(aPos, 1.0);
+    ourColor = aColor; // set ourColor to the input color we got from the vertex data
+}  
+)glsl";
+
+const char* fragmentShaderSource = R"glsl(
+#version 330 core
+out vec4 FragColor;  
+in vec3 ourColor;
+  
+void main()
+{
+    FragColor = vec4(ourColor, 1.0);
+}
+)glsl";
 
 int main(void)
 {
-    GLFWwindow* window;
-    if (!glfwInit()) // Initialize GLFW
-        return -1;
-
-    // Set OpenGL to 3.3 Core
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    /* Initialize GLFW */ if (!glfwInit())
+    {
+        std::cout << "Failed to initialize GLFW" << std::endl;
+		return -1;
+    }
+    /* Set OpenGL to 3.3 Core */ {
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); 
+    }
 
     // Create window, 1920x1080
-    window = glfwCreateWindow(1920, 1080, "App", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(1920, 1080, "App", NULL, NULL);
     if (!window)
     {
+        std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
         return -1;
     }
+
     glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+    glfwSetKeyCallback(window, keyCallback);
+    
+    // glfwSwapInterval(1);
 
-    // V-Sync on
-    glfwSwapInterval(1);
-
-    // Initialize GLEW, check for errors, print OpenGL version
-    if (glewInit() != GLEW_OK)
-        std::cout << "Error!" << std::endl;
-    else
-        std::cout << glGetString(GL_VERSION) << std::endl;
-
-
-    // Scope bug fix for persistent terminal
+    /* Initialize GLEW */ if (glewInit())
     {
-        // Enable Alpha Blending
-        GLCall(glEnable(GL_BLEND));
-        GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
+        std::cout << "Error!" << std::endl;
+        return -1;
+    }
+    std::cout << glGetString(GL_VERSION) << std::endl;
+    int success; char infoLog[512];
+    
+    // Vertex shader
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
 
-        Renderer renderer;
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
 
-        // Initialize ImGui
-        ImGui::CreateContext();
-        ImGui_ImplGlfw_InitForOpenGL(window, true);
-        ImGui_ImplOpenGL3_Init((char*)glGetString(GL_NUM_SHADING_LANGUAGE_VERSIONS));
-        ImGui::StyleColorsDark();
+    // Fragment shader
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
 
-        // Test Menu
-        test::Test* currentTest = nullptr;
-        test::TestMenu* testMenu = new test::TestMenu(currentTest);
-        currentTest = testMenu;
+    // Link shaders
+    unsigned int shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
 
-        testMenu->RegisterTest<test::TestClearColor>("Clear Color");
-        testMenu->RegisterTest<test::TestTex2D>("2D Texture");
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+    }
 
-        // Main Loop
-        while (!glfwWindowShouldClose(window))
-        {
-            // Clear Screen
-            GLCall(glClearColor(0.0f, 0.0f, 0.0f, 1.0f));
-            renderer.Clear();
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
 
-            // ImGui New Frame
-            ImGui_ImplOpenGL3_NewFrame();
-            ImGui_ImplGlfw_NewFrame();
-            ImGui::NewFrame();
+    float vertices[] = {
+     // Positions           Colors
+        0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // Bottom right
+       -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // Bottom left
+        0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // Top
+    };
 
-            // Test Menu
-            if (currentTest)
-            {
-                currentTest->OnUpdate(0.0f);
-                currentTest->OnRender();
-                ImGui::Begin("Test");
-                if (currentTest != testMenu && ImGui::Button("<-"))
-				{
-					delete currentTest;
-					currentTest = testMenu;
-				}
-                currentTest->OnImGuiRender();
-                ImGui::End();
-            }
+    unsigned int indices[] = {
+        0, 1, 2,  // First triangle
+    };
 
-            // ImGui Render
-            ImGui::Render();
-            ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+    unsigned int VBO, VAO, IBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &IBO);
+    glBindVertexArray(VAO);
 
-            // Swap Buffers and Poll Events
-            glfwSwapBuffers(window);
-            glfwPollEvents();
-        }
-        // Delete Test Menu when Main Loop ends
-        delete currentTest;
-        if (currentTest != testMenu)
-			delete testMenu;
-	}
-    // Close Window at End of Program
-    ImGui_ImplOpenGL3_Shutdown();
-    ImGui_ImplGlfw_Shutdown();
-    ImGui::DestroyContext();
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    glfwTerminate();
-    return 0;
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    // position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    // color attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+
+    ImGui::CreateContext();
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init((char*)glGetString(GL_NUM_SHADING_LANGUAGE_VERSIONS));
+    ImGui::StyleColorsDark();
+
+
+    // Main Loop
+    while (!glfwWindowShouldClose(window))
+    {
+        // Render
+        glClearColor(0.2f, 0.3f, 0.8f, 0.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        if (isWireframe)
+            glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+        else
+            glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+
+
+        // ImGui New Frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+
+        glUseProgram(shaderProgram);
+
+        float timeValue = glfwGetTime();
+        float greenValue = sin(timeValue) / 2.0f + 0.5f;
+        int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+        glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+
+        // ImGui Test
+        ImGui::Text("(%.1f FPS)", ImGui::GetIO().Framerate);
+
+        // ImGui Render
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+
+        // Swap Buffers and Poll Events
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    {
+        glDeleteVertexArrays(1, &VAO);
+        glDeleteBuffers(1, &VBO);
+        glDeleteBuffers(1, &IBO);
+        glDeleteProgram(shaderProgram);
+        ImGui_ImplOpenGL3_Shutdown();
+        ImGui_ImplGlfw_Shutdown();
+        ImGui::DestroyContext();
+        glfwTerminate();
+        return 0; 
+    }
 }
